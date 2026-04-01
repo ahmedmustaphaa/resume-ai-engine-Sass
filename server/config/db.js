f3c9c0ad5f6dac3513cc2b17b3dd1097dd49b45c
Ahmed Mustafa <ahmed@example.com>
2026-04-01 18:51:25 +0200

    up[date in mongoose

--- server/config/db.js
+++ server/config/db.js
@@ -1,14 +1,11 @@
 import mongoose from 'mongoose';
 import 'dotenv/config'; // لو شغال ES Modules
 
-export const connectedDb=async()=>{
-    try{
-
-        console.log(process.env.MONGOOSE_URL)
+export const connectedDb = async () => {
+    try {
         await mongoose.connect(process.env.MONGOOSE_URL);
-        console.log("mongoose are working")
-
-    }catch(error){
-        console.log(error)
+        console.log("Database Connected Successfully! ✅"); // دي مش هتطبع إلا لو الاتصال نجح فعلاً
+    } catch (error) {
+        console.error("Database Connection Error ❌:", error.message);
     }
 }
